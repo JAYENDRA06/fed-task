f024a0dbc5f656a2447f53e51e05ab863d3d4374
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2023-05-09 23:59:48 +0530

    fix: minor css mistakes

--- src/components/subComponents/VideoInformation.jsx
+++ src/components/subComponents/VideoInformation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function VideoInformation({ title }) {
   return (
     <div className="d-flex justify-content-between my-3">
-      <h5>{title}</h5>
+      <h5 className="text-xl text-lg">{title}</h5>
       <div className="d-flex justify-content-between w-auto">
       <button className="btn video-btn-outlined btn-outline-primary">Previous</button>
         <button className="btn btn-primary video-btn">Next</button>
